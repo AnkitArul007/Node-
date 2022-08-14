7f4a6e2fa84a4126e8b4a5269f918ec465f13bdf
Ankit <nktarul@gmail.com>
2022-08-14 21:19:25 +0530

    change6

--- My1stProj/index.js
+++ My1stProj/index.js
@@ -1,21 +1,35 @@
 
 const express = require('express');
+const port = process.env.PORT || 5000; // our port number where our server will be created
 const bodyParser = require('body-parser');
-const app = express();
+const db = require('mongoose');
+const app = express();  // this is our app or instance of express
+const fs = require('fs');
+
+
+//API middlewares::
 
 app.use(bodyParser.json());
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));  // this is to decode the data send through html form
+
+//API routes::
 
 app.get('/login', (req, res)=>{
     res.sendFile('view/index.html', {root : __dirname})
 });
 
 app.post('/form', (req, res)=>{
-    console.log(req.body)
-    res.send('data-recieved')
+    console.log(req.body); // the data we get is in body of the request
+    res.send('data-recieved');
 
+    fs.appendFile('text.txt', JSON.stringify(req.body).toLocaleLowerCase(), (err)=>{
+        if (err) throw err;
+        console.log("successful");
+    })
 });
 
-app.listen('5000', ()=>{
+app.listen(port, ()=>{
     console.log('server started');
 });
 
